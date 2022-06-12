63f3d84c1000521be8e1204cf3e4e76e97542eb7
Mehmet Demirel <exortek979@gmail.com>
2022-06-12 21:17:49 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
-const express = require('express');
+const express = require( 'express' );
 const app = express();
-require('dotenv').config({
-    path: './config/config.env'
-})
-const {PORT} = process.env;
-const bodyParser = require('body-parser');
-const {graphqlHTTP} = require('express-graphql');
+require( 'dotenv' ).config( {
+    path : './config/config.env'
+} )
+const { PORT } = process.env;
+const bodyParser = require( 'body-parser' );
+const { graphqlHTTP } = require( 'express-graphql' );
 const connectDatabase = require( "./helpers/database/connectDatabase" );
-const graphQlSchema = require('./graphql/schema/index');
-const graphQlResolvers = require('./graphql/resolvers/index');
+const graphQlSchema = require( './graphql/schema/index' );
+const graphQlResolvers = require( './graphql/resolvers/index' );
+const isAuth = require( './middlewares/auth/auth' );
 connectDatabase();
-app.use('/graphql', graphqlHTTP({
-    schema: graphQlSchema,
-    rootValue: graphQlResolvers,
-    graphiql: true,
-}))
-app.use(bodyParser.json());
+app.use( isAuth );
+app.use( '/graphql',graphqlHTTP( {
+    schema : graphQlSchema,
+    rootValue : graphQlResolvers,
+    graphiql : true,
+} ) )
+app.use( bodyParser.json() );
 
 
-app.listen(PORT || 5001, () => {
-    console.log(`Server is running on port ${PORT} -> http://localhost:${PORT}`);
-})
+app.listen( PORT || 5001,() => {
+    console.log( `Server is running on port ${PORT} -> http://localhost:${PORT}` );
+} )
